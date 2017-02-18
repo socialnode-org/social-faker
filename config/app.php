a3c255fc9ceed44302f45a686f75046d5f9ceea2
Daniel Supernault <danielsupernault@gmail.com>
2017-02-18 12:39:47 -0700

    Add the fractal package
    Fractal provides a presentation and transformation layer for complex
    data output, the like found in RESTful APIs, and works really well with
    JSON. Think of this as a view layer for your JSON/YAML/etc.

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Spatie\Fractal\FractalServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -225,6 +226,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Fractal' => Spatie\Fractal\FractalFacade::class,
 
     ],
 
