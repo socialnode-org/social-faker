f594d93637e75a6414cdb697bc8d7e0603c3e938
Daniel Supernault <danielsupernault@gmail.com>
2017-02-18 15:04:29 -0700

    Renamed example feature test to RouteTest

--- tests/Feature/RouteTest.php
+++ tests/Feature/RouteTest.php
@@ -7,17 +7,29 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class RouteTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testHomeTest()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function testAllFeedTest()
+    {
+        $response = $this->get('/main/all');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'error' => 404, 
+                'msg' => 'Not yet implemented'
+            ]);
     }
 }
