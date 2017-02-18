ee11597e6a8cf282b13c772f5f0f44762d434d58
Daniel Supernault <danielsupernault@gmail.com>
2017-02-18 16:33:37 -0700

    Add methods to Notice model

--- app/Notice.php
+++ app/Notice.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notice extends Model
 {
-    //
+
+	public function user() 
+	{
+		return $this->belongsTo('App\User');
+	}
+
+    public function uri() 
+    {
+    	$tag = 'example.org';
+    	$timestamp = $this->created_at->format('Y-M-d');
+    	$id = $this->id;
+    	return "tag:$tag,$timestamp:noticeId=$id:objectType=note";
+    }
+
 }
