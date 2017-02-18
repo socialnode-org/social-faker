8daafa2a13ad6f9dd1629cde42454cd2f409ad0a
Daniel Supernault <danielsupernault@gmail.com>
2017-02-18 16:34:14 -0700

    Add public timeline controller method

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,7 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use League\Fractal\Manager;
+use League\Fractal\Resource\Collection;
+use App\{Notice,User};
+use App\Social\Transformers\{PublicTimelineTransformer};
 class ApiController extends Controller
 {
-    //
+    public function publicTimeline($fmt = 'json')
+    {
+    	$notices = Notice::orderBy('id', 'desc')->take(10)->get();
+		$manager = new Manager();
+		$resource = new Collection($notices, new PublicTimelineTransformer());
+		$res = $manager->createData($resource)->toArray();
+
+		return response()->json($res['data'], 200, [], JSON_PRETTY_PRINT);
+    }
 }
