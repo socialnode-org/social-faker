9e299cb3fdb2a7dfe5affa9406a7600dae925ef9
Daniel Supernault <danielsupernault@gmail.com>
2017-02-18 19:30:18 -0700

    Update uri() method on Notice model

--- app/Notice.php
+++ app/Notice.php
@@ -12,12 +12,31 @@ class Notice extends Model
 		return $this->belongsTo('App\User');
 	}
 
-    public function uri() 
+    public function uri($objectType = "note") 
     {
     	$tag = 'example.org';
     	$timestamp = $this->created_at->format('Y-M-d');
     	$id = $this->id;
-    	return "tag:$tag,$timestamp:noticeId=$id:objectType=note";
+
+    	switch ($objectType) {
+    		case 'note':
+    			$res = "tag:$tag,$timestamp:noticeId=$id:objectType=$objectType";
+    			break;
+    		case 'thread':
+    			$res = "tag:$tag,$timestamp:objectType=$objectType:nonce=".str_random(16);
+    			break;
+    		
+    		default:
+    			$res = "tag:$tag,$timestamp:noticeId=$id:objectType=$objectType";
+    			break;
+    	}
+    	return $res;
+    }
+
+    public function url()
+    {
+    	$domain = config('app.url');
+    	return "{$domain}/notice/{$this->id}";
     }
 
 }
